987e5311ea071c5909fa0f2332c468e36098562c
Sten Govaerts <sten.govaerts@gmail.com>
2026-04-15 13:52:18 +0200

    Remove faulty phpdoc vars from question response

--- src/Contracts/SurveyQuestionResponseContract.php
+++ src/Contracts/SurveyQuestionResponseContract.php
@@ -9,12 +9,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property int $id
  * @property int $surveyhero_question_id
  * @property int $surveyhero_answer_id
- * @property string $field
- * @property string $converted_string_value
- * @property string $converted_int_value
+ * @property int $survey_response_id
  * @property Carbon $created_at
  * @property Carbon $updated_at
- * @property int $survey_response_id
  * @property SurveyResponseContract $surveyResponse
  * @property SurveyQuestionContract $surveyQuestion
  * @property SurveyAnswerContract $surveyAnswer

cbfe9d49fb87c3911fd9fc63192fcb471402fd4e
Sten Govaerts <sten.govaerts@gmail.com>
2026-07-01 16:46:58 +0200

    Fix github changelog action Fix phpstan error

--- src/Services/Factories/ResponseCreator/FileResponseCreator.php
+++ src/Services/Factories/ResponseCreator/FileResponseCreator.php
@@ -49,20 +49,20 @@ class FileResponseCreator extends AbstractQuestionResponseCreator
         $surveyQuestion = $this->findSurveyQuestion($surveyheroQuestionResponse->element_id);
 
         $filePath = $surveyheroQuestionResponse->file->path ?? null;
-        if ($filePath === null) {
-            return $this->createSurveyQuestionResponseData($surveyQuestion, $response, null);
+
+        $surveyAnswer = null;
+        if ($filePath !== null) {
+            $apiUrl = (new SurveyheroConfig)->getApiUrl();
+            $scheme = parse_url($apiUrl, PHP_URL_SCHEME);
+            $host = parse_url($apiUrl, PHP_URL_HOST);
+            $fileUrl = ($scheme && $host) ? "{$scheme}://{$host}{$filePath}" : $filePath;
+
+            $surveyAnswer = $this->fetchOrCreateInputAnswer(
+                $surveyQuestion,
+                SurveyAnswerContract::CONVERTED_TYPE_STRING,
+                $fileUrl
+            );
         }
-
-        $apiUrl = (new SurveyheroConfig)->getApiUrl();
-        $scheme = parse_url($apiUrl, PHP_URL_SCHEME);
-        $host = parse_url($apiUrl, PHP_URL_HOST);
-        $fileUrl = ($scheme && $host) ? "{$scheme}://{$host}{$filePath}" : $filePath;
-
-        $surveyAnswer = $this->fetchOrCreateInputAnswer(
-            $surveyQuestion,
-            SurveyAnswerContract::CONVERTED_TYPE_STRING,
-            $fileUrl
-        );
 
         $responseData = $this->createSurveyQuestionResponseData($surveyQuestion, $response, $surveyAnswer);
 
